5bb9890158ceae17e953f87a319545900c2dbbb4
許景皓 <estherhsu@Chin-Haos-MacBook-Pro.local>
2017-11-03 14:21:20 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Chin-Hao Hsu on 2017/11/3.
+//  Copyright © 2017年 Chin-Hao Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
